f2885b486dcddbff2c41623b631185afae6b7cb8
Logan Wilkins <lswilkins@uncg.edu>
2025-10-09 12:38:33 -0400

    Templete setup

---
+++ src/main/java/com/example/CSC_340/Practice/API/Csc340PracticeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.CSC_340.Practice.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csc340PracticeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csc340PracticeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CSC_340/Practice/API/Csc340PracticeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CSC_340.Practice.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csc340PracticeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
